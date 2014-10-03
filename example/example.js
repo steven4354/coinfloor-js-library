0df5be2ffff10bc373f3c37c5b93b088538d616b
rorygreig <rorygreig@gmail.com>
2014-10-03 13:00:19 +0100

    web socket working

--- example/example.js
+++ example/example.js
@@ -1,10 +1,12 @@
-var Coinfloor = require('../coinfloor_api.js');
+var Coinfloor = require('../Coinfloor.js');
 var credentials = require('./testCredentials.js');
 
 var coinfloor = new Coinfloor(credentials.coinfloorID, credentials.password, credentials.api_key);
 
-coinfloor.getTicker(function(err, res){
-    if(err) throw(err);
+console.log(coinfloor.connect());
 
-    console.log(res);
-});
+// coinfloor.getTicker(function(err, res){
+//     if(err) throw(err);
+//
+//     console.log(res);
+// });
